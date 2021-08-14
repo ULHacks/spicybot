6415004ed32d908c39f8612de0bc69dcdd08aab4
George Zhang <geetransit@gmail.com>
2021-08-13 23:01:03 -0400

    Handle on_message

--- bot.py
+++ bot.py
@@ -11,4 +11,8 @@ client = discord.Client()
 async def on_ready():
     print(f"Bot logged in as {client.user.name}")
 
+@client.event
+async def on_message(message):
+    await message.channel.send("Got a message!")
+
 client.run(os.environ["BOT_TOKEN"])
