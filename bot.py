20d5cb4ec6dcc65accdc9474cc6bef4e1a2939d1
George Zhang <geetransit@gmail.com>
2021-08-13 23:14:44 -0400

    Add documentation

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ async def on_message(message):
         return
     await message.channel.send("Got a message!")
 
-@client.command(name="12345")
+@client.command(name="12345", help="Continues the nursery rhyme")
 async def nursery_rhyme(ctx):
     await ctx.message.channel.send("Once I caught a fish alive!")
 
